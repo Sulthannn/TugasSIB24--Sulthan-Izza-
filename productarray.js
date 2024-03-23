37a35651b96a4844351086a26783ee24daf701f9
Sulthan Izza Falqahi <sulthanizza11@gmail.com>
2024-03-23 21:28:39 +0700

    Tugas8

---
+++ productarray.js
@@ -0,0 +1,56 @@
+let fishProduct = [ 
+    {
+     name: 'Tuna',
+     price: 48000,
+     deskripsi: 'sehat',
+     image: 'https://cdn1-production-images-kly.akamaized.net/ywf4JSdbqm_wrMR7zEP04lCOH-s=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3362679/original/062243900_1611887906-Tuna__1_.jpg'
+    },
+    {
+     name: 'Mackerel',
+     price: 35000,
+     deskripsi: 'lembut',
+     image: 'https://www.oceansnorth.org/wp-content/uploads/2019/04/shutterstock_337409645.jpg'
+    },
+    {
+     name: 'Sardine',
+     price: 25000,
+     deskripsi: 'gurih',
+     image: 'https://www.mensjournal.com/.image/t_share/MTk2MTM3MTk1ODg5MTA4NDg1/two-sardine-on-white-background-close-up-elevated-view.jpg'
+    },
+    {
+     name: 'Salmon',
+     price: 55000,
+     deskripsi : 'enak',
+     image: 'https://s3.bukalapak.com/bukalapak-kontenz-production/content_attachments/websites/10/96403/original/Jenis_Ikan_Salmon_Di_Indonesia.jpg'
+    },  
+    {
+     name: 'Halibut',
+     price: 85000,
+     deskripsi: 'daging putih',
+     image: 'https://www.hi.no/resources/imr/nyheter/2017/04/kveiteaveivindsenneset.jpg/articleimg'
+    },
+    {
+     name: 'Barramundi',
+     price: 70000,
+     deskripsi: 'mantap',
+     image: 'https://cdn.idntimes.com/content-images/community/2022/03/1024px-caught-barramundi-97ae06e75a95eb7595641ab869500840-9807c426075bb75c5a13bf7bc7d69dfc.JPG'
+    },
+    {
+     name: 'Cumi-Cumi',
+     price: 65000,
+     deskripsi: 'hidangan favorit',
+     image: 'https://imgx.parapuan.co/crop/0x0:0x0/x/photo/2023/04/01/cara-olah-cumi-agar-hasilnya-tid-20230401055445.jpg'
+    },
+    {
+     name: 'Kerang Dara',
+     price: 50000,
+     deskripsi: 'kenyal',
+     image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Anadara_granosa.jpg'
+    },
+    {
+     name: 'Udang',
+     price: 44000,
+     deskripsi: 'maknyus',
+     image: 'https://asset-2.tstatic.net/travel/foto/bank/images/udang-mentah.jpg'
+    }  
+];
